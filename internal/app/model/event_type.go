0ee47911d089a61a91a693476f829292e184ecc2
Dima Oshuev <mazafon64@gmail.com>
2025-05-30 23:18:18 +0300

    models

--- internal/app/model/event_type.go
+++ internal/app/model/event_type.go
@@ -1,12 +1,10 @@
 package model
 
 type EventType struct {
-	EventTypeID uint   `gorm:"primaryKey;autoIncrement" json:"event_type_id"`
-	Name        string `gorm:"size:100;not null" json:"name"`
-
-	// Associations
-	Events []Event     `gorm:"foreignKey:EventTypeID" json:"events,omitempty"`
-	Shows  []EventShow `gorm:"foreignKey:EventTypeID" json:"shows,omitempty"`
+	EventTypeID uint        `gorm:"primaryKey;autoIncrement" json:"event_type_id"`
+	Name        string      `gorm:"size:100;not null" json:"name"`
+	Events      []Event     `gorm:"foreignKey:EventTypeID" json:"events,omitempty"`
+	Shows       []EventShow `gorm:"foreignKey:EventTypeID" json:"shows,omitempty"`
 }
 
 func (EventType) TableName() string {
--- internal/app/model/resident_interest.go
+++ internal/app/model/resident_interest.go
@@ -1,1 +1,12 @@
 package model
+
+type ResidentInterest struct {
+	ResidentID uint     `gorm:"primaryKey" json:"resident_id"`
+	InterestID uint     `gorm:"primaryKey" json:"interest_id"`
+	Resident   Resident `gorm:"foreignKey:ResidentID" json:"resident"`
+	Interest   Interest `gorm:"foreignKey:InterestID" json:"interest"`
+}
+
+func (ResidentInterest) TableName() string {
+	return "resident-interest"
+}
